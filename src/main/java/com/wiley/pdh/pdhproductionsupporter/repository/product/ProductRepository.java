da94a3ea613cef40dd1402937010205d0a675000
pgeeganage <pgeeganage@wiley.com>
2023-11-13 19:19:09 +0530

    Refactor the code

--- src/main/java/com/wiley/pdh/pdhproductionsupporter/repository/product/ProductRepository.java
+++ src/main/java/com/wiley/pdh/pdhproductionsupporter/repository/product/ProductRepository.java
@@ -11,23 +11,7 @@ import java.math.BigInteger;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, BigInteger> {
 
-//    @Procedure(name = "pdh_utils_pkg.mark_delete_product")
-//    Object markDeleteProductProcedure(@Param("p_dh_prod_id") BigInteger dhProdId,
-//                                      @Param("p_ticket_name") String ticketId);
-
-//    @Modifying
-//    @Query(value = "BEGIN pdh_utils_pkg.mark_delete_product(:p_dh_prod_id, trim(:p_ticket_name)); END;", nativeQuery = true)
-//    void markDeleteProductProcedure(@Param("p_dh_prod_id") BigInteger dhProdId,
-//                                    @Param("p_ticket_name") String ticketId);
-
-//    @Query(value = "{ CALL pdh_utils_pkg.mark_delete_product(:p_dh_prod_id, trim(:p_ticket_name)) }", nativeQuery = true)
-
     @Procedure(name = "Product.markDeleteProductProcedure")
     void markDeleteProductProcedure(@Param("p_dh_prod_id") BigInteger dhProdId,
                                     @Param("p_ticket_name") String ticketId);
-
-//    @Modifying
-//    @Query(value = "BEGIN merge_util.process_source(trim(:in_src_sys)); END;", nativeQuery = true)
-//    void executeSurvivorShip(@Param("in_src_sys") String sourceSystem);
-//    @Query(value = "{ CALL merge_util.process_source(trim(:in_src_sys)) }", nativeQuery = true)
 }
